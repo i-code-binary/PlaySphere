b0610a58654d76622c6788e4f9f5cdf3563c86bd
Aditya Raj <adi.adityakohli2005@gmail.com>
2025-01-01 00:04:52 +0530

    added TermsCondition Page

--- src/components/PaymentPage.tsx
+++ src/components/PaymentPage.tsx
@@ -1,9 +1,17 @@
 "use client";
 
+import { useSession } from "next-auth/react";
 import PaymentForm from "./PaymentForm";
 import { StarsBackground } from "./ui/StarsBackground";
+import { useEffect } from "react";
+import { useRouter } from "next/navigation";
 
 export default function PaymentPage() {
+  const { data: session } = useSession();
+  const router = useRouter();
+  useEffect(() => {
+    if (!session || !session.user) router.push("/authentication");
+  }, []);
   return (
     <div className="flex justify-center items-center min-h-screen bg-black">
       <PaymentForm />
