0de5187cf29a9d502da2ab17e5b786c98d57795d
Aditya Raj <adi.adityakohli2005@gmail.com>
2025-01-02 17:38:30 +0530

    removed orderid storage problem

--- src/app/payment-verification/page.tsx
+++ src/app/payment-verification/page.tsx
@@ -15,14 +15,13 @@ export default function Page() {
           router.push("/payment-cancel?error=missing_token");
         }
         const response = await axios.post(`/api/verify-payment`, { token });
+        localStorage.removeItem('ORDER_ID')
         if (response?.data?.url) {
           router.push(response.data.url);
         } else router.push(`/payment-cancel?error=payment_failed`);
       } catch (error) {
         console.log(error)
         router.push("/payment-cancel?error=something_went_wrong");
-      }finally{
-        localStorage.removeItem('ORDER_ID')
       }
     }
     init();
